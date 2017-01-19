2515eebb85d90dbf5ad1bc1130a9804c6e63df13
werner.holzapfel <werner.holzapfel@trivento.nl>
2017-01-19 20:11:46 +0100

    advertentie toegevoegd

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-import {NavController, PopoverController, ViewController} from "ionic-angular";
+import {NavController, PopoverController, ViewController, Platform} from "ionic-angular";
 import {Nummereentotaalstand} from "../../models/nummereentotaalstand";
 import {Homepageprovider} from "../../providers/homepageprovider";
 import {LaatsteupdateProvider} from "../../providers/laatsteupdateprovider";
@@ -8,8 +8,9 @@ import {Nummereenteamstandlaatsteronde} from "../../models/Nummereenteamstandlaa
 import {Subscription} from "rxjs";
 import {DropdownmenuPage} from "../dropdownmenu/dropdownmenu";
 import {Headlines} from "../../models/headlines"
-import { SpinnerDialog } from 'ionic-native';
+import { SpinnerDialog} from 'ionic-native';
 
+declare var AdMob: any;
 
 /*
  Generated class for the Home page.
@@ -30,13 +31,72 @@ export class HomePage {
   headline : Headlines;
   LaatsteupdateSub: Subscription;
   headlinesSub: Subscription;
+  private admobId: any;
 
   constructor(public navCtrl: NavController,
               public viewCtrl: ViewController,
               private homepageProvider: Homepageprovider,
               private laatsteupdateProvider: LaatsteupdateProvider,
-              public popoverCtrl: PopoverController) {
+              public popoverCtrl: PopoverController,
+              private platform: Platform) {
+
+    this.platform = platform;
+    if(/(android)/i.test(navigator.userAgent)) {
+      this.admobId = {
+        banner: 'ca-app-pub-4938627038388421/4360126395',
+        interstitial: 'ca-app-pub-4938627038388421/5557657998'
+      };
+    } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+      this.admobId = {
+        banner: 'ca-app-pub-4938627038388421/4360126395',
+        interstitial: 'ca-app-pub-4938627038388421/5557657998'
+      };
+    }
   }
+
+  createBanner() {
+    this.platform.ready().then(() => {
+      if(AdMob) {
+        AdMob.createBanner({
+          adId: this.admobId.banner,
+          position: AdMob.AD_POSITION.BOTTOM_CENTER,
+          isTesting: true,
+          autoShow: true
+        });
+      }
+    });
+  }
+
+  // showInterstitial() {
+  //   this.platform.ready().then(() => {
+  //     if(AdMob) {
+  //       AdMob.prepareInterstitial({
+  //         adId: this.admobId.interstitial,
+  //         autoShow: true
+  //       });
+  //     }
+  //   });
+  // }
+  //
+  // showBanner(position) {
+  //   this.platform.ready().then(() => {
+  //     if(AdMob) {
+  //       let positionMap = {
+  //         "bottom": AdMob.AD_POSITION.BOTTOM_CENTER,
+  //         "top": AdMob.AD_POSITION.TOP_CENTER
+  //       };
+  //       AdMob.showBanner(positionMap[position.toLowerCase()]);
+  //     }
+  //   });
+  // }
+  //
+  // hideBanner(position) {
+  //   this.platform.ready().then(() => {
+  //     if(AdMob) {
+  //       AdMob.hideBanner();
+  //     }
+  //   });
+  // }
 
   ionViewWillEnter() {
     if(!this.nummereentotaalstand) SpinnerDialog.show();
@@ -61,7 +121,9 @@ export class HomePage {
     this.headlinesSub = this.homepageProvider.getheadlines().subscribe(response => {
       // console.log(response);
       this.headline = response[0]
-    })
+    });
+
+    this.createBanner();
   }
 
   ionViewWillLeave() {
@@ -73,12 +135,12 @@ export class HomePage {
 
   ionViewDidLoad() {
     console.log('Hello HomePage Page');
+    AdMob.onAdDismiss()
+      .subscribe(() => { console.log('User dismissed ad'); });
   }
 
   presentPopover(event) {
     let popover = this.popoverCtrl.create(DropdownmenuPage);
     popover.present();
   }
-
-
 }
